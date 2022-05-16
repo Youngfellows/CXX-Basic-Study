b95e2dfc8f4eed7e3e8dd00cd4bf404bfb25211a
Your Name <you@example.com>
2022-05-16 22:07:48 +0800

    xxxx

--- lesson23/example07/Sample.cpp
+++ lesson23/example07/Sample.cpp
@@ -73,11 +73,17 @@ void test3()
     cout << myStr->get() << endl;//获取字符串
   
 
-    int upper;//大写字母
-    int lower;//小写字母
-    int digit;//数字
-    int space;//空格
-    int other;//其他字符
+    int upper = 0;//大写字母
+    int lower = 0;//小写字母
+    int digit = 0;//数字
+    int space = 0;//空格
+    int other = 0;//其他字符
+    // cout << "test3():: 大写字母:" << upper << endl;
+    // cout << "test3():: 小写字母:" << lower << endl;
+    // cout << "test3():: 数字字符:" << digit << endl;
+    // cout << "test3():: 空格字符:" << space << endl;
+    // cout << "test3():: 其他字符:" << other << endl;
+
     myStr->calc(calculate1,ps,upper,lower,digit,space,other);//通过函数指针调用回调函数
     cout << "大写字母:" << upper << endl;
     cout << "小写字母:" << lower << endl;
@@ -132,6 +138,11 @@ void calculate1(char *str,int &upper,int &lower,int &digit,int &space,int &other
 {
     cout << "calculate1() 回调函数执行 ..." << endl;
     cout << "calculate1():: str=" << str << endl;
+    // cout << "calculate1()::1,大写字母:" << upper << endl;
+    // cout << "calculate1()::1,小写字母:" << lower << endl;
+    // cout << "calculate1()::1,数字字符:" << digit << endl;
+    // cout << "calculate1()::1,空格字符:" << space << endl;
+    // cout << "calculate1()::1,其他字符:" << other << endl;
     char *p = str;//指针变量p指向字符串str
     while (*p != '\0')
     {
@@ -139,28 +150,28 @@ void calculate1(char *str,int &upper,int &lower,int &digit,int &space,int &other
        //cout << ch << endl;
        if(ch >= 'A' && ch <= 'Z')
        {
-            upper = upper + 1;
+            upper++;
        }else if (ch >= 'a' && ch <= 'z')
        {
-            lower = lower + 1;
+            lower++;
        }else if (ch >= '0' && ch <= '9')
        {
             digit++;
        }else if(isspace(ch))//是空格判断
        {
-            cout << ch << "是空格,space=" << space << endl;
-            space = space + 1;
+            //cout << ch << "是空格,space=" << space << endl;
+            space++;
        }else{
-            cout << ch << "是特殊字符,other=" << other << endl;
-            other = other + 1;
+            //cout << ch << "是特殊字符,other=" << other << endl;
+            other++;
        }
        p++;//移动指针指向
     }
-    cout << "calculate1()::大写字母:" << upper << endl;
-    cout << "calculate1()::小写字母:" << lower << endl;
-    cout << "calculate1()::数字字符:" << digit << endl;
-    cout << "calculate1()::空格字符:" << space << endl;
-    cout << "calculate1()::其他字符:" << other << endl;
+    // cout << "calculate1()::大写字母:" << upper << endl;
+    // cout << "calculate1()::小写字母:" << lower << endl;
+    // cout << "calculate1()::数字字符:" << digit << endl;
+    // cout << "calculate1()::空格字符:" << space << endl;
+    // cout << "calculate1()::其他字符:" << other << endl;
 
 }
 
@@ -185,17 +196,17 @@ void calculate2(char *str,int *upper,int *lower,int *digit,int *space,int *other
            (*digit)++;
        }else if (isspace(ch))//是空格判断
        {
-            cout << ch << "是空格,space=" << *space << endl;
+            //cout << ch << "是空格,space=" << *space << endl;
             (*space)++;
        }else{
-            cout << ch << "是特殊字符,other=" << *other << endl;
+            //cout << ch << "是特殊字符,other=" << *other << endl;
             (*other)++;
        }
        p++;//移动指针指向
     }
-    cout << "calculate2()::大写字母:" << *upper << endl;
-    cout << "calculate2()::小写字母:" << *lower << endl;
-    cout << "calculate2()::数字字符:" << *digit << endl;
-    cout << "calculate2()::空格字符:" << *space << endl;
-    cout << "calculate2()::其他字符:" << *other << endl;
+    // cout << "calculate2()::大写字母:" << *upper << endl;
+    // cout << "calculate2()::小写字母:" << *lower << endl;
+    // cout << "calculate2()::数字字符:" << *digit << endl;
+    // cout << "calculate2()::空格字符:" << *space << endl;
+    // cout << "calculate2()::其他字符:" << *other << endl;
 }
