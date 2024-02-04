c718fedada9d374cf6bb2d6b0014651ed7929760
Youngfellow <Youngfellow@163.com>
2024-02-04 17:26:35 +0800

    xxxxxxx

--- 02-CXX-Basic/01-varargs/src/Main.cpp
+++ 02-CXX-Basic/01-varargs/src/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "VariableArgument.h"
 
-int main() {
+int main(int argc, char **argv) {
     std::cout << "Hello, World!" << std::endl;
     printNumbers(5, 1, 2, 3, 4, 5);
     return 0;
